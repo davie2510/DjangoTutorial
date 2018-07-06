b9c8f6283f8775450543ba9c908671c35815ea2b
david <david.diop.frimpong@gmail.com>
2018-07-06 19:12:03 +0000

    1st commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\david\Documents\Python Projects\DjangoTutorial\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
